611869b56e2f242a39e8028aa2c852448710d205
nicoescandarani <nico@escandarani.com.ar>
2020-03-09 01:57:26 -0300

    Styling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const WHITE_KEYS = ['z', 'x', 'd', 'v', 'b', 'n', 'm'];
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
 
 const keys = document.querySelectorAll('.key');
